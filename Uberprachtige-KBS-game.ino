baf84050c1d1eeb353dac14cb145db711f32b8de
BastogneBas <11506433+BastogneBas@users.noreply.github.com>
2019-01-06 16:30:50 +0100

    Revert ":octocat:"
    This reverts commit 3cc88af7cfef8f021d49d88982c5551857bde08d.

--- Uberprachtige-KBS-game.ino
+++ Uberprachtige-KBS-game.ino
@@ -215,23 +215,21 @@ int main()
 //				Definitions::irComm->write(Definitions::irComm->read());
 //			}
 			if (Serial.available()){
-				//char buffer[64] = {0};
-				//Serial.readBytes(buffer, Serial.available());
-				//Serial.println(buffer);
-				//Definitions::irComm->print(buffer);
-				//Serial.println("Done");
-				Definitions::irComm->write(Serial.read());
+				char buffer[64] = {0};
+				Serial.readBytes(buffer, Serial.available());
+				Serial.println(buffer);
+				Definitions::irComm->print(buffer);
+				Serial.println("Done");
 			}
 		
 			if(Definitions::irComm->available())
 			{
-				//Serial.print(Definitions::irComm->available());
-				//Serial.print("\t");
-				//Serial.print(Definitions::irComm->peek(), HEX);
-				//Serial.print("\t");
-				//Serial.write(Definitions::irComm->read());
-				//Serial.println();
+				Serial.print(Definitions::irComm->available());
+				Serial.print("\t");
+				Serial.print(Definitions::irComm->peek(), HEX);
+				Serial.print("\t");
 				Serial.write(Definitions::irComm->read());
+				Serial.println();
 				//Serial.print("\t");
 				//Definitions::irComm->println(Definitions::irComm->available());
 			}
